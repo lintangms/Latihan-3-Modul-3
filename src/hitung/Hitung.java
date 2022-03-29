1241652ed63f8cfde8d176a040e2cc66bd46e268
Lintang MS <lintangmuhammadshiddiq@gmail.com>
2022-03-29 07:37:18 +0700

    first commit

---
+++ src/hitung/Hitung.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitung;
+
+/**
+ *
+ * @author User
+ */
+public class Hitung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BangunDatar hitunglah=new BangunDatar();
+        
+        hitunglah.luas(2, 5, 6);
+        hitunglah.luas(3.5, 6.9);
+    
+
+    }
+    
+}
